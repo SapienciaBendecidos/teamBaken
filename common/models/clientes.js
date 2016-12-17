7181a2fc34ce369edd62053f56db40544723c61e
rNexeR <rnexer@gmail.com>
2016-12-16 23:40:24 -0600

    Get Clients with sum of their cards

--- common/models/clientes.js
+++ common/models/clientes.js
@@ -3,27 +3,18 @@
 var app = require('../../server/server');
 
 module.exports = function(Clientes, Tarjetas) {
-    function createUser(primerNombre, segundoNombre, primerApellido, segundoApellido, telefono, cb) {
-      Clientes.create([
-        {primerNombre: primerNombre, segundoNombre: segundoNombre, primerApellido: primerApellido, segundoApellido: segundoApellido, telefono: telefono},
-      ], cb);
-    }
-	function createCard(idCliente, _saldo, cb) {
-	  app.models.Tarjetas.create([
-	    {saldo: _saldo, estado: "active", idCliente: idCliente}
-	  ], cb);
-	}
-    function getClientsWithSaldo(idCliente, cb){
+
+    function getClientWithSaldo(idCliente, cb){
         app.models.Clientes.findOne({where: {idCliente: idCliente}}, function(err, client){
             if(err) cb("Client not Found", null);
             else{
                 app.models.Tarjetas.find({where: {idCliente: idCliente}}, function(err, cards){
                     if(err) cb(err, null);
                     else{
-                        console.log(cards);
-                        var saldo = 0;
-                        for (var i = 0; i < cards.length; i++){
-                            console.log(cards[i]);
+                        //console.log(cards);
+                        let saldo = 0;
+                        for (let i = 0; i < cards.length; i++){
+                            //console.log(cards[i]);
                             saldo += cards[i].saldo;
                         }
                         client.saldo = saldo;
@@ -34,46 +25,30 @@ module.exports = function(Clientes, Tarjetas) {
         })
     }
 
-    Clientes.getWithSaldo = function(idCliente, cb){
-        getClientsWithSaldo(idCliente, function(err, ret){
+    Clientes.getWithSaldo = function(cb){
+        let sql_st =  `Select
+                            c.*, s.total
+                        from clientes c
+                        inner join (Select sum(t.saldo) as total, t.id_cliente
+                            from tarjetas t
+                            group by t.id_cliente) s
+                            
+                            on s.id_cliente = c.id_cliente;
+    `;
+        app.datasources.mysqlDs.connector.execute(sql_st, null, function(err, data){
             if(err) cb(err, null);
-            else cb(null, ret);
-        });
-    }
-
-    Clientes.createClient = function(primerNombre, segundoNombre, primerApellido, segundoApellido, telefono, saldo, cb) {
-	createUser(primerNombre, segundoNombre, primerApellido, segundoApellido, telefono, function(err, data) {
-            if (!err) {
-                createCard(data[0].idCliente, saldo, function(error) {
-                    if (error) cb(null, error);
-                    else cb(null, "Created");
-            	})
-            }else{
-                cb(null, "User not created, Error: " + err);
+            else{
+                //console.log(data);
+                cb(null,data);
             }
         });
     }
-    Clientes.remoteMethod('createClient', {
-        accepts:[
-            {arg: 'primerNombre', type: 'string'},
-            {arg: 'segundoNombre', type: 'string'},
-            {arg: 'primerApellido', type: 'string'},
-            {arg: 'segundoApellido', type: 'string'},
-            {arg: 'telefono', type: 'string'},
-            {arg: 'saldo', type: 'number'},
-            ],
-        returns: {arg: 'createClient', type: 'string'},
-        http: {path: '/createClient', verb: 'post'}
+
+    Clientes.remoteMethod('getWithSaldo', {
+        returns: {arg: 'getWithSaldo', type: 'Object'},
+        http: {path: '/getWithSaldo', verb: 'get'}
         }
     );
 
-    Clientes.remoteMethod('getWithSaldo', {
-        accepts:[
-            {arg: 'idCliente', type: 'number', required: true}
-            ],
-        returns: {arg: 'getWithSaldo', type: 'Object'},
-        http: {path: '/:id/WithSaldo', verb: 'get'}
-        }
-    );
 };
 
