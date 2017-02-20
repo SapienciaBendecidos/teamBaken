cc2753e0cc98aa1ea53a8df1e54611f053ca1da5
Jesus Murillo <chusmurillo9@gmail.com>
2017-02-20 12:16:50 -0600

    User activations

--- server/boot/create_seed.js
+++ server/boot/create_seed.js
@@ -11,9 +11,9 @@ module.exports = function(app) {
     {
 
         app.models.Users.create([
-            { username: "nasty", firstName: 'Brandon', firstSurname: 'Napkin', password: "mipassword", email: "nasky@hotmail.com"},
-            { username: "joshua", firstName: 'Josue', firstSurname: 'Enamorado', password: "mipassword", email: "joshua@hotmail.com"},
-            { username: "chus", firstName: 'Chungo', firstSurname: 'Murillo', password: "mipassword", email: "chungo@hotmail.com"},
+            { username: "nasty", firstName: 'Brandon', firstSurname: 'Napkin', password: "mipassword", email: "nasky@hotmail.com", status: "active"},
+            { username: "joshua", firstName: 'Josue', firstSurname: 'Enamorado', password: "mipassword", email: "joshua@hotmail.com", status: "active"},
+            { username: "chus", firstName: 'Chungo', firstSurname: 'Murillo', password: "mipassword", email: "chungo@hotmail.com", status: "active"},
             ], function(err, users) {
                
                 if (err) throw err;
