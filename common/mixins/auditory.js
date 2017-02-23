fddfea15f12c38da4d5df3af32bad5ba513d0c86
Jose34mp <antonio.mperdomo21@gmail.com>
2017-02-22 23:10:00 -0600

    Update and Create Time

--- common/mixins/auditory.js
+++ common/mixins/auditory.js
@@ -2,12 +2,11 @@
 var loopback = require('loopback');
 var LoopBackContext = require('loopback-context');
 
-module.exports = function(Model, options) {
+module.exports = function(Model) {
 		Model.observe('before save', function(ctx, next){
-		var ctx = LoopBackContext.getCurrentContext();
-		var currentUser = ctx && ctx.get('currentUser');
-	  	Model.defineProperty('UserCreate', {type: Number, default: currentUser.id});
-	  	Model.defineProperty('UserUpdate', {type: Number, default: currentUser.id});
+		var user = LoopBackContext.getCurrentContext();
+		var currentUser = ctx && user.get('currentUser');
+    	console.log('currentUser.username: ', currentUser.id); // voila!
     	next();
     });
 
