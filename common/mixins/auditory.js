8d71d3fa3e297185e62c5b7af6571c5fac86551e
Jose34mp <antonio.mperdomo21@gmail.com>
2017-02-23 12:08:54 -0600

    hiden update and create

--- common/mixins/auditory.js
+++ common/mixins/auditory.js
@@ -4,10 +4,8 @@ var LoopBackContext = require('loopback-context');
 
 module.exports = function(Model) {
 		Model.observe('before save', function(ctx, next){
-		var user = LoopBackContext.getCurrentContext();
-		var currentUser = ctx && user.get('currentUser');
-    	console.log('currentUser.username: ', currentUser.id); // voila!
-    	next();
+		var user = ctx.options.remoteCtx.accessToken.userId;
+		next();
     });
 
 }
--- server/server.js
+++ server/server.js
@@ -40,7 +40,41 @@ app.start = function() {
     }
   });
 };
+function inject(ctx, next) {
+  var options = hasOptions(ctx.method.accepts) && (ctx.args.options || {});
+  if(options) {
+    options.remoteCtx = ctx;
+    ctx.args.options = options;
+  }
+  next();
+}
 
+app.remotes().before('*.*', inject);
+
+app.remotes().before('*.prototype.*', function(ctx, instance, next) {
+  inject(ctx, next);
+});
+
+// unfortunately this requires us to add the options object
+// to the remote method definition
+app.remotes().methods().forEach(function(method) {
+  if(!hasOptions(method.accepts)) {
+    method.accepts.push({
+      arg: 'options',
+      type: 'object',
+      injectCtx: true
+    });
+  }
+});
+
+function hasOptions(accepts) {
+  for (var i = 0; i < accepts.length; i++) {
+    var argDesc = accepts[i];
+    if (argDesc.arg === 'options' && argDesc.injectCtx) {
+      return true;
+    }
+  }
+}
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
