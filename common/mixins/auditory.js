e08c30b87e45d5e8f30519f63b377ee8670ed68f
rNexeR <rnexer@gmail.com>
2017-02-23 22:42:03 -0600

    mixin of auditory

--- common/mixins/auditory.js
+++ common/mixins/auditory.js
@@ -5,6 +5,12 @@ module.exports = function(Model) {
 	Model.observe('before save', function(ctx, next){
 		//var user = ctx.options.remoteCtx.accessToken.userId;
 		//console.log(user);
+		//console.log(JSON.stringify(ctx, null, 2));
+		//console.log(ctx.hookState);
+		console.log(ctx.req);
+		for (var i = 0; i < Object.keys(ctx.Model).length - 1; i++) {
+			console.log(Object.keys(ctx.Model)[i]);	
+		}
 		/*try{
 			console.log(ctx.options.remoteCtx.req.loopbackContext.active.accessToken.userId);
 			if(ctx.isNewInstance){
@@ -15,6 +21,7 @@ module.exports = function(Model) {
 			console.log(e);
 		}*/
 		//console.log(ctx.req);
+		//console.log(ctx);
 		next();
 	});
 
--- common/models/equipos-servicio.js
+++ common/models/equipos-servicio.js
@@ -1,3 +1,10 @@
 'use strict';
 module.exports = function(Equiposservicio) {
+	Equiposservicio.beforeRemote('create', function(ctx, next){
+		console.log(ctx.req.headers);
+		console.log("access token: ", ctx.req.query.access_token);
+		for (var i = Object.keys(ctx.req).length - 1; i >= 0; i--) {
+			console.log(Object.keys(ctx.req)[i]);
+		}
+	})
 };
