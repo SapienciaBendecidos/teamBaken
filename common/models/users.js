43a58fd6b978489f8e876299838251f849486096
rNexeR <rnexer@gmail.com>
2017-02-26 01:50:09 -0600

    ?

--- common/models/users.js
+++ common/models/users.js
@@ -3,47 +3,55 @@ var app = require('../../server/server');
 
 module.exports = function(Users) {
 	Users.observe('after save', function(ctx, next){
+		console.log("{"+ctx.instance.type+"}", ctx.instance.type == "admin");
 		if(ctx.isNewInstance && ctx.instance.type !== undefined){
 			console.log("after create");
 			app.models.Role.findOne({where:{name: ctx.instance.type}}, function(err, role) {
 				if (err) throw err;
+				else if(!role){
 
-				role.principals.create({
-					principalType: app.models.RoleMapping.USER,
-					principalId: ctx.instance.id
-				}, function(err, principal) {
-					if(err) throw err;
-					else{
-						next();
-					}
-				});
-			});
-		}else if(ctx.instance.type !== undefined){
-			console.log("after update");
-			app.models.Role.findOne({ where: {name: ctx.instance.type}}, function(err, role) {
-			if (err) throw err;
-
-			console.log(role);
-
-			app.models.RoleMapping.findOne({where: {principalType: "USER", principalId: ctx.instance.id}}, function(err, mapping){
-				if (err) throw err;
-				else if(!mapping){
+				}else{
 					role.principals.create({
 						principalType: app.models.RoleMapping.USER,
 						principalId: ctx.instance.id
 					}, function(err, principal) {
 						if(err) throw err;
-						else next();
+						else{
+							next();
+						}
 					});
+				}
+			});
+		}else if(ctx.instance.type !== undefined){
+			console.log("after update");
+			app.models.Role.findOne({ where: {name: ctx.instance.type}}, function(err, role) {
+				if (err) throw err;
+				else if(!role){
+
 				}else{
-					console.log(ctx.instance, role, mapping);
-					app.dataSources.mysqlDs.connector.execute("update SBO.RoleMapping set roleId = ? where id = ?", [role.id, mapping.id], function(err, doc){
+					app.models.RoleMapping.findOne({where: {principalType: "USER", principalId: ctx.instance.id}}, function(err, mapping){
 						if (err) throw err;
-						else next();
+						else if(!mapping){
+							role.principals.create({
+								principalType: app.models.RoleMapping.USER,
+								principalId: ctx.instance.id
+							}, function(err, principal) {
+								if(err) throw err;
+								else next();
+							});
+						}else{
+							console.log(ctx.instance, role, mapping);
+							/*mapping.roleId = role.id;
+							mapping.save();
+							next();*/
+							app.dataSources.mysqlDs.connector.execute("update SBO.RoleMapping set roleId = ? where id = ?", [role.id, mapping.id], function(err, doc){
+								if (err) throw err;
+								else next();
+							})
+						}
 					})
 				}
-			})
-		});
+			});
 		}else{
 			next();
 		}
